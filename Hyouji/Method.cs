8bb244549a48f87c2e95ed0a15fa71c6000bc75b
寄能光貴 <k_kino@carekarte.jp>
2023-09-22 21:39:33 +0900

    'コミットしたよ'

---
+++ Hyouji/Method.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyouji
+{
+    internal class Method
+    {
+        string filePath = "SaveText.txt";
+        public void Savetext(string TextContents)
+        {
+            using(StreamWriter Writer = new StreamWriter(filePath,true))
+            {
+                Writer.WriteLine(TextContents);
+            }
+        }
+    }
+}
